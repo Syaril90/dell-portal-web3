cd9e335349686cfb1cc4947dbc4b6c9576820619
Syaril Nazirul Sazali <syarilnazirul.sazali@Syarils-MBP.realtek>
2022-04-03 11:49:11 +0800

    get data from api and server side rendering

--- pages/index.js
+++ pages/index.js
@@ -1,109 +1,15 @@
 import { ChevronLeftIcon } from "@heroicons/react/solid";
+import axios from "axios";
 import ProductCard from "../components/ProductCard"
 import Header from '../components/Header';
 import HeaderNav from "../components/HeaderNav";
 
-let products = [
-  {
-    id: 1,
-    name: "OptiPlex 3090 Small Form Factor Desktop - Build Your Own",
-    imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxctBMy1rexNv_Adne8qdqa4F-cVnK6a014TFXrBm_l73d27YzpJfG6NDUFJ3JYlJv_V8&usqp=CAU",
-    ratingStar: 5,
-    ratingNumber: 333,
-    normalPrice: 2222,
-    actualPrice: 1111,
-    cpu: "Acceleration Use Only Intel® Core™ i3-10105T",
-    ram: "8 GB, 1 x 8 GB, DDR4",
-    disk: "2.5 inch 500GB 7200 RPM Hard Disk Drive, 128 MB cache",
-    windows: "Windows 11 Home Single Language (Dell Technologies recommends Windows 11 Pro for business)",
-    description: "Essential commercial micro form factor desktop that delivers best-in-class security and performance. Built secure with TPM 2.0, standard security lock slot, padlock loop, intrusion switch, and SED hard drive.",
-    orderCode: "kjhdsfiosdhfoidfi"
-  },
-  {
-    id: 2,
-    name: "OptiPlex 3090 Small Form Factor Desktop - Build Your Own",
-    imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxctBMy1rexNv_Adne8qdqa4F-cVnK6a014TFXrBm_l73d27YzpJfG6NDUFJ3JYlJv_V8&usqp=CAU",
-    ratingStar: 5,
-    ratingNumber: 333,
-    normalPrice: 2222,
-    actualPrice: 1111,
-    cpu: "Acceleration Use Only Intel® Core™ i3-10105T",
-    ram: "8 GB, 1 x 8 GB, DDR4",
-    disk: "2.5 inch 500GB 7200 RPM Hard Disk Drive, 128 MB cache",
-    windows: "Windows 11 Home Single Language (Dell Technologies recommends Windows 11 Pro for business)",
-    description: "Essential commercial micro form factor desktop that delivers best-in-class security and performance. Built secure with TPM 2.0, standard security lock slot, padlock loop, intrusion switch, and SED hard drive."
-  },
-  {
-    id: 3,
-    name: "OptiPlex 3090 Small Form Factor Desktop - Build Your Own",
-    imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxctBMy1rexNv_Adne8qdqa4F-cVnK6a014TFXrBm_l73d27YzpJfG6NDUFJ3JYlJv_V8&usqp=CAU",
-    ratingStar: 5,
-    ratingNumber: 333,
-    normalPrice: 2222,
-    actualPrice: 1111,
-    cpu: "Acceleration Use Only Intel® Core™ i3-10105T",
-    ram: "8 GB, 1 x 8 GB, DDR4",
-    disk: "2.5 inch 500GB 7200 RPM Hard Disk Drive, 128 MB cache",
-    windows: "Windows 11 Home Single Language (Dell Technologies recommends Windows 11 Pro for business)",
-    description: "Essential commercial micro form factor desktop that delivers best-in-class security and performance. Built secure with TPM 2.0, standard security lock slot, padlock loop, intrusion switch, and SED hard drive."
-  }, {
-    id: 4,
-    name: "OptiPlex 3090 Small Form Factor Desktop - Build Your Own",
-    imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxctBMy1rexNv_Adne8qdqa4F-cVnK6a014TFXrBm_l73d27YzpJfG6NDUFJ3JYlJv_V8&usqp=CAU",
-    ratingStar: 5,
-    ratingNumber: 333,
-    normalPrice: 2222,
-    actualPrice: 1111,
-    cpu: "Acceleration Use Only Intel® Core™ i3-10105T",
-    ram: "8 GB, 1 x 8 GB, DDR4",
-    disk: "2.5 inch 500GB 7200 RPM Hard Disk Drive, 128 MB cache",
-    windows: "Windows 11 Home Single Language (Dell Technologies recommends Windows 11 Pro for business)",
-    description: "Essential commercial micro form factor desktop that delivers best-in-class security and performance. Built secure with TPM 2.0, standard security lock slot, padlock loop, intrusion switch, and SED hard drive."
-  }, {
-    id: 5,
-    name: "OptiPlex 3090 Small Form Factor Desktop - Build Your Own",
-    imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxctBMy1rexNv_Adne8qdqa4F-cVnK6a014TFXrBm_l73d27YzpJfG6NDUFJ3JYlJv_V8&usqp=CAU",
-    ratingStar: 5,
-    ratingNumber: 333,
-    normalPrice: 2222,
-    actualPrice: 1111,
-    cpu: "Acceleration Use Only Intel® Core™ i3-10105T",
-    ram: "8 GB, 1 x 8 GB, DDR4",
-    disk: "2.5 inch 500GB 7200 RPM Hard Disk Drive, 128 MB cache",
-    windows: "Windows 11 Home Single Language (Dell Technologies recommends Windows 11 Pro for business)",
-    description: "Essential commercial micro form factor desktop that delivers best-in-class security and performance. Built secure with TPM 2.0, standard security lock slot, padlock loop, intrusion switch, and SED hard drive."
-  }, {
-    id: 6,
-    name: "OptiPlex 3090 Small Form Factor Desktop - Build Your Own",
-    imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxctBMy1rexNv_Adne8qdqa4F-cVnK6a014TFXrBm_l73d27YzpJfG6NDUFJ3JYlJv_V8&usqp=CAU",
-    ratingStar: 5,
-    ratingNumber: 333,
-    normalPrice: 2222,
-    actualPrice: 1111,
-    cpu: "Acceleration Use Only Intel® Core™ i3-10105T",
-    ram: "8 GB, 1 x 8 GB, DDR4",
-    disk: "2.5 inch 500GB 7200 RPM Hard Disk Drive, 128 MB cache",
-    windows: "Windows 11 Home Single Language (Dell Technologies recommends Windows 11 Pro for business)",
-    description: "Essential commercial micro form factor desktop that delivers best-in-class security and performance. Built secure with TPM 2.0, standard security lock slot, padlock loop, intrusion switch, and SED hard drive."
-  }, {
-    id: 7,
-    name: "OptiPlex 3090 Small Form Factor Desktop - Build Your Own",
-    imagePath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxctBMy1rexNv_Adne8qdqa4F-cVnK6a014TFXrBm_l73d27YzpJfG6NDUFJ3JYlJv_V8&usqp=CAU",
-    ratingStar: 5,
-    ratingNumber: 333,
-    normalPrice: 2222,
-    actualPrice: 1111,
-    cpu: "Acceleration Use Only Intel® Core™ i3-10105T",
-    ram: "8 GB, 1 x 8 GB, DDR4",
-    disk: "2.5 inch 500GB 7200 RPM Hard Disk Drive, 128 MB cache",
-    windows: "Windows 11 Home Single Language (Dell Technologies recommends Windows 11 Pro for business)",
-    description: "Essential commercial micro form factor desktop that delivers best-in-class security and performance. Built secure with TPM 2.0, standard security lock slot, padlock loop, intrusion switch, and SED hard drive."
-  }
-]
 
 
-export default function Home() {
-  let previousPage = "Shop Laptop Computers & 2-in-1 PCs"
+export default function Home({ productsData }) {
+
+  const previousPage = "Shop Laptop Computers & 2-in-1 PCs"
+
   return (
     <div className="h-screen w-full overflow-hidden overflow-y-scroll">
       <Header />
@@ -113,10 +19,18 @@ export default function Home() {
         <div>{previousPage}</div>
       </div>
       <div className="p-5 flex flex-wrap space-x-3">
-        {products.map((item) => (
+        {productsData.map((item) => (
           <ProductCard key={item.id} product={item} />
         ))}
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  var productsData = await axios.get('http://localhost:3000/api/products').data;
+
+  return {
+    props: { productsData }
+  }
 }
